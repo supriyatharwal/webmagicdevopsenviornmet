a508eb1656a36340c907678be15b051045e6fa6a
CodeCommit-at-948995554136 <info@webmagicinformatica.com>
2020-07-03 18:07:07 +0530

    changed image

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker network create --driver bridge devopsnetwork
 docker build . -t jenkins -f jenkins/Dockerfile
-docker run -itd -p 8080:8080 --rm --name jenkins -v jenkins_volume:/var/jenkins_home --net devopsnetwork jenkins/jenkins:latest
+docker run -itd -p 8080:8080 --rm --name jenkins -v jenkins_volume:/var/jenkins_home --net devopsnetwork jenkins
+docker exec jenkins cat /var/jenkins_home/secrets/initialAdminPassword
