fd58e0ac0f9716824ac2d404f153e1e0831906d1
CodeCommit-at-948995554136 <info@webmagicinformatica.com>
2020-07-03 10:48:50 +0530

    Update jenkins Image

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+docker network create --driver bridge devopsnetwork
 docker build . -t jenkins -f jenkins/Dockerfile
+docker run -itd -p 8080:8080 --rm --name jenkins -v jenkins_volume:/var/jenkins_home --net devopsnetwork jenkins/jenkins:latest
