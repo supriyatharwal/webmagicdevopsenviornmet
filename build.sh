ad86d83b109d918bef8cba5360f2361f5d4c70e2
CodeCommit-at-948995554136 <info@webmagicinformatica.com>
2020-07-09 01:48:36 +0530

    gcloud support work

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ docker network create --driver bridge devopsnetwork
 docker build . -t jenkins -f jenkins/Dockerfile
 docker run -itd -p 8080:8080 --rm --name jenkins -v jenkins_volume:/var/jenkins_home --net devopsnetwork jenkins
 docker exec jenkins cat /var/jenkins_home/secrets/initialAdminPassword
+docker run -itd --rm --name sonarqube -p 9000:9000 --net devopsnetwork -v sonarqube:/opt/sonarqube/data sonarqube
---
+++ jenkins/script/gcloud.sh
@@ -0,0 +1,4 @@
+echo "deb http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+apt-get update && apt-get install google-cloud-sdk -y
+gcloud auth login
